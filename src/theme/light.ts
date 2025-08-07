757178abfd22b7da2bdf9849dd1f7322232badbf
nian <tenianon@gmail.com>
2025-08-07 12:30:47 +0800

    fix: slider tooltip in light theme style

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -167,8 +167,11 @@ export function baseLightThemeOverrides(primaryColor = ''): GlobalThemeOverrides
       },
     },
     Popover: {
-      color: twc.neutral[25],
-      textColor: 'rgb(51, 54, 57)',
+      textColor: light.textColor2,
+    },
+    Slider: {
+      indicatorColor: twc.neutral[25],
+      indicatorTextColor: light.textColor2,
     },
     Select: {
       peers: {
