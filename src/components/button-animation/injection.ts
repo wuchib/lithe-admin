7e80c9826b42c25c25bc66f864f8ec16071c5584
nian <tenianon@gmail.com>
2025-08-21 09:23:42 +0800

    fix: add symbolic name to buttonAnimationInjectionKey to improve readability

--- src/components/button-animation/injection.ts
+++ src/components/button-animation/injection.ts
@@ -1,4 +1,4 @@
 import type { ButtonAnimationProps } from './interface'
 import type { InjectionKey } from 'vue'
 
-export const buttonAnimationInjectionKey = Symbol() as InjectionKey<Partial<ButtonAnimationProps>>
+export const buttonAnimationInjectionKey: InjectionKey<Partial<ButtonAnimationProps>> = Symbol('buttonAnimationInjectionKey')
